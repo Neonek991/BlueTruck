617d78ed87bce8cf118b23b973638aec508c962a
CurlyBrackets <Neonek991@gmail.com>
2017-03-09 17:49:11 +0100

    Commit layout

--- app/src/main/java/eu/curlybrackets/bluetruck/BlueTruck.java
+++ app/src/main/java/eu/curlybrackets/bluetruck/BlueTruck.java
@@ -1,6 +1,6 @@
 package eu.curlybrackets.bluetruck;
-
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+
 public class BlueTruck extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class BlueTruck extends AppCompatActivity {
         setContentView(R.layout.activity_blue_truck);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
