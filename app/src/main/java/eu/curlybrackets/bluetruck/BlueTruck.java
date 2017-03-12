bc6ae1405e4da10c7697a1bc135a1821ef20822b
CurlyBrackets <Neonek991@gmail.com>
2017-03-12 15:05:17 +0100

    SCANN DEVICES CLASS

--- app/src/main/java/eu/curlybrackets/bluetruck/BlueTruck.java
+++ app/src/main/java/eu/curlybrackets/bluetruck/BlueTruck.java
@@ -1,19 +1,34 @@
 package eu.curlybrackets.bluetruck;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 
 public class BlueTruck extends AppCompatActivity {
 
+    //Check Bluetooth on/off
+    public BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    ListView list;
 
+    //SCANN DEVICES
+    public ArrayList<String> BTDeviceList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,18 @@ public class BlueTruck extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Check Bluetooth on/off
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        onResume();
+
+
+        //SCANN DEVICES
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        //list reference to founded devices
+        list = (ListView)findViewById(R.id.ScannDevicesList);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +56,29 @@ public class BlueTruck extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
+                //start bluetooth search devices
+
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                mBluetoothAdapter.startDiscovery();
+
+
+
+
             }
         });
+    }
+
+
+    //Check Bluetooth on/off
+    protected void onResume(){
+        super.onResume();
+
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableintent, REQUEST_ENABLE_BT);
+        }
     }
 
     @Override
@@ -53,4 +101,30 @@ public class BlueTruck extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    //SCANN DEVICES
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+
+
+                BTDeviceList.clear();
+
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+
+                BTDeviceList.add(device.getName() + "\n" + device.getAddress());
+                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                list.setAdapter(new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, BTDeviceList));
+
+                list.invalidateViews();
+
+                String a = new String(BTDeviceList.toString());
+                Log.i("All",a);
+
+
+            }}};
 }
---
+++ app/src/main/java/eu/curlybrackets/bluetruck/ScanDevices.java
@@ -0,0 +1,30 @@
+package eu.curlybrackets.bluetruck;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by neonek on 12.03.17.
+ */
+
+
+
+public class ScanDevices extends BlueTruck{
+
+
+
+
+
+
+
+
+}
